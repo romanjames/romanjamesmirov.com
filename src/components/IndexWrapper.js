1070c3915c9ec0f1e746e813a48aa245e1b69465
Roman James Mirov <romanjamesmirov@gmail.com>
2020-08-30 02:06:36 -0700

    Revamp.

--- src/components/IndexWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import "aos/dist/aos.css"
-import "./index-wrapper.scss"
-
-export default function IndexWrapper({ children }) {
-  return <React.Fragment>{children}</React.Fragment>
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,109 @@
-import React, { useState, useEffect } from "react"
-import IndexWrapper from "../components/IndexWrapper"
-import AOS from "aos"
-import styles from "./index.module.scss"
+import React from "react"
+import { Helmet } from "react-helmet"
+
 import ProfileImage from "../../static/profile-400x400.jpg"
 import CodeThinkingImage from "../../static/code-thinking.svg"
 import BloggingImage from "../../static/blogging.svg"
 
 export default function Home() {
-  useEffect(() => {
-    AOS.init()
-    // document.addEventListener("scroll", () => setScrollSpanOpacity(styles.))
-  }, [])
-
   return (
-    <IndexWrapper>
-      <section className={styles.helloSection}>
-        <h1>hi! i am roman.</h1>
+    <div id="index-page">
+      <Helmet bodyAttributes={{ class: "orange-background" }} />
+
+      <h1 className="hello">
+        Hello! My name is Roman. <br />
+        Nice to meet you.
+      </h1>
+
+      <section className="hand-wave">
+        <h2>👋🏼</h2>
+      </section>
+
+      <section className="check-it-out">
+        <h2>OK, so check this out:</h2>
+        <figure>
+          <a href="#">
+            <div></div>
+          </a>
+          <figcaption>
+            I made this blogging platform for a social worker who wants to share
+            her stories about interesting people.{" "}
+          </figcaption>
+        </figure>
         <p>
-          <span>scroll </span>
-          <span role="img" aria-label="Finger pointing down emoji">
-            👇
-          </span>
+          See it live at <a href="#">allyseweaver.com</a>
         </p>
       </section>
-      <section className={styles.bioCardSection}>
-        <div>
-          <div>
-            <img src={ProfileImage} alt="My profile image" />
-          </div>
-          <div>
-            <h3>@romanjamesmirov</h3>
-            <p>Web Developer Specializing in Blogs and Social Platforms</p>
-          </div>
-        </div>
+
+      <section className="reach-out">
+        <h2>I'm a developer of blogs.</h2>
+        <p>
+          Would you like one for yourself? I'm <span>@romanjamesdev</span> on
+          every social network.
+          <br />
+          <br />
+          You can also reach me here:{" "}
+        </p>
+        <form action="post">
+          <input type="email" name="email" placeholder="Your email" />
+          <textarea name="message" rows="10" placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
       </section>
-      <section className={styles.descriptionSection}>
-        <h2 className={styles.bigH2}>i build blogs.</h2>
-        <div>
-          <p>
-            designing and programming
-            <br /> websites from scratch
-            <br /> is my thing.
-          </p>
-          <img src={CodeThinkingImage} alt="Thinking about websites in code" />
-        </div>
-      </section>
-      <section className={styles.socialWorkerBlogSection}>
-        <h2 className={styles.bigH2}>
-          here's one i created for a social worker
-        </h2>
+
+      <section className="showcase">
+        <h2>...my portfolio also includes</h2>
         <div>
           <a href="#">
-            <i>☼</i>allyseweaver.com
+            <i>☼</i>staysafesacramento.com
           </a>
           <a href="#">
             <figure>
               <div></div>
               <figcaption>
-                Stories about people. From a long-time social worker.
+                a pandemic relief platform to connect people who have and people
+                who need
               </figcaption>
             </figure>
           </a>
         </div>
-        <img src={BloggingImage} alt="Writing a blog post" />
+        <div>
+          <a href="#">
+            "blogster"<i>☼</i>
+          </a>
+          <a href="#">
+            <figure>
+              <div></div>
+              <figcaption>
+                a minimalist... you guessed it... blogging platform. go ahead,
+                make a test account and post something.
+              </figcaption>
+            </figure>
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <i>☼</i>"impetus"
+          </a>
+          <a href="#">
+            <figure>
+              <div></div>
+              <figcaption>
+                toggle on and off the tasks you do throughout the day and see
+                auto-generated charts showing where your time goes
+              </figcaption>
+            </figure>
+          </a>
+        </div>
       </section>
-    </IndexWrapper>
+      <footer>
+        <a href="#">go back up</a>
+        <div>
+          twitter github linkedin youtube instagram codepen medium facebook
+          pinterest
+        </div>
+        <p>© Roman Mirov 2020</p>
+      </footer>
+    </div>
   )
 }
